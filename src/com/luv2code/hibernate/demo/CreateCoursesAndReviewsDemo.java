70a304e99a4478fcf347bf026115f1e7550eab33
Vitor Silva <vitorgilcs@gmail.com>
2019-12-27 19:37:26 +0000

    Hibernate Advanced Mapping - @OneToMany - Unidirectional

---
+++ src/com/luv2code/hibernate/demo/CreateCoursesAndReviewsDemo.java
@@ -0,0 +1,64 @@
+
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+import com.luv2code.hibernate.demo.entity.Review;
+import com.luv2code.hibernate.demo.entity.course;
+
+
+public class CreateCoursesAndReviewsDemo  {
+
+	public static void main(String[] args) {
+		
+		//create session factory
+		SessionFactory factory = new Configuration()
+							.configure("hibernate.cfg.xml")
+							.addAnnotatedClass(Instructor.class)
+							.addAnnotatedClass(InstructorDetail.class)
+							.addAnnotatedClass(course.class)
+							.addAnnotatedClass(Review.class)
+							.buildSessionFactory();
+		
+		//create session
+		Session session = factory.getCurrentSession();
+
+		//use the session object to save Java object		
+		try {
+			//start a transaction
+			session.beginTransaction();
+			
+			//create a course
+			course tempCourse = new course("Pacman - How to Score One Million Points");
+			
+			//add some reviews
+			tempCourse.addReview(new Review("Great course ... loved it! "));
+			tempCourse.addReview(new Review("Good course, job well done "));
+			tempCourse.addReview(new Review("What a dumb course, you are an idiot!"));
+			
+			//save the course ... and leverage the cascade all 
+			System.out.println("Saving the course");
+			System.out.println(tempCourse);
+			System.out.println(tempCourse.getReviews());
+		
+			session.save(tempCourse);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally{
+			//add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
